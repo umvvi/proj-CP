8fe68a207fbe660d4a3b3c7fcbde95ecb0ec3f3b
BleuSkell <kyasowi1@gmail.com>
2024-02-06 11:01:55 +0100

    Kyano DB gemaakt, v2

---
+++ proj-CP/KyanoDB/confDatabase/conf.php
@@ -0,0 +1,9 @@
+<?php
+$dbHost = 'localhost';
+
+$dbName = 'Project-P3';
+
+$dbUser = 'Project-P3-L1';
+
+$dbPass = 'ProjectP3L1';
+?>
--- proj-CP/KyanoDB/questionaire.php
+++ proj-CP/KyanoDB/questionaire.php
@@ -17,13 +17,35 @@
 
         //maak een databse aan. 1:start wamp op, 2:zoek op je browser "localhost", 3:ga naar de localhost van wamp,
         //4:ga naar "php myadmin" in "your aliases", 5:log in met de password: ProjectP3L1, username: Project-P3-L1,
-        //6:klik dan op "nieuw", 7:noem de database: Project-P3, 8:noem de tabelnaam "Questionaire" en maak 2 kolommen,
+        //6:klik dan op "nieuw", 7:noem de database: Project-P3, 8:noem de tabelnaam "Questionaire" en maak 3 kolommen,
         //9:eerste kolom: "naam: ID" ; "type: INT" ; "lengte: 500" ; "standaardwaarde: geen" ; "collatie: geen" ; "attributen: geen"
         //"leeg: niet aanvinken" ; "index: Primary" ; "al het andere laten"
         //10:tweede kolom: "naam: e-mail" ; "type: varchar" ; "lengte: 100" ; "standaardwaarde: geen" ; "collatie: geen" ; "attributen: geen"
         //"leeg: niet aanvinken" ; "index: geen" ; "al het andere laten"
-        //11:derde kolom: "naam: e-mail" ; "type: varchar" ; "lengte: 100" ; "standaardwaarde: geen" ; "collatie: geen" ; "attributen: geen"
+        //11:derde kolom: "naam: answers" ; "type: varchar" ; "lengte: 500" ; "standaardwaarde: geen" ; "collatie: geen" ; "attributen: geen"
         //"leeg: niet aanvinken" ; "index: geen" ; "al het andere leeg laten"
+
+        include('confDatabase/conf.php');
     ?>
+    <h1>Fill in this form</h1>
+
+    <form action="create.php" method="post">
+        <label for="firstname">Firstname:</label>
+        <input type="text" name="firstname" id="firstname"><br><br>
+
+        <label for="tussenvoegsel">Infix:</label>
+        <input type="text" name="infix" id="infix"><br><br>
+
+        <label for="lastname">Lastname:</label>
+        <input type="text" name="lastname" id="lastname"><br><br>
+
+        <label for="birthday">Birth-date:</label>
+        <input type="date" name="birthday" id="birthday"><br><br>
+
+        <label for="city">City:</label>
+        <input type="text" name="city" id="city"><br><br>
+
+        <input type="submit" value="Submit">
+    </form>
 </body>
 </html>
